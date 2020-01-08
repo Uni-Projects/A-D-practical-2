e27292b9e4523eaf2b991cd4469846e4fa5a736f
Paul2803k <p.scattolin95@gmail.com>
2020-01-08 10:02:06 +0100

    something that actually works

--- main.cpp
+++ main.cpp
@@ -2,177 +2,139 @@
  * Paolo Scattolin
  * Johan Urban
  */
-#include <iostream>
 #include <vector>
+#include <queue>
+#include <bits/stdc++.h>
 #include <string>
 #include <map>
-#include <chrono>
-#include <sstream>
 
 using namespace std;
-using namespace std::chrono;
 
-struct Actor {
-    string name;
-    map<int, string> moviesMap;
-};
+#define MAX 1000000
+#define NIL 0
+#define INF 999999
 
-struct Actress {
-    string name;
-    vector<string> starredmovies;
-};
+vector<int> graph[MAX];
+int n, match[MAX], dist[MAX];
 
-vector<Actress> actresses;
-vector<Actor> actors;
+map<int, int> actorsMap;
+map<int, int> actressesMap;
 
-//this function returns true if actress and actor have costarred in some movie, false otherwise
-//every movie the actress is in is checked on every actor hash-map to see if they costarred
-bool costarred (const Actress& actress , const Actor& actor){
-    hash<string> hash;
-    for (const string& m : actress.starredmovies){
-        auto it = actor.moviesMap.find(hash(m));
-        if ( it != actor.moviesMap.end())
-            return true;
+bool bfs() {
+    int i, u, v, len;
+    queue<int> Q;
+    for (i = 1; i <= n; i++) {
+        if (match[i] == NIL) {
+            dist[i] = 0;
+            Q.push(i);
+        } else dist[i] = INF;
     }
-    return false;
-}
-
-bool bpm(vector<vector<bool>>& bpGraph, int u, vector<bool>& seen, vector<int>& matchR){
-    // Try every actor one by one
-    for (int v = 0; v < bpGraph.size(); v++){
-        // If actress u has worked with actor v and v is not been already seen
-        if (bpGraph[u][v] && !seen[v]){
-            // Mark v as seen
-            seen[v] = true;
-            // If actor 'v' is not already matched to an
-            // actresses OR previously seen, actress for actor v (which is matchR[v])
-            // has an alternate actor available. ???
-            // Since v is marked as visited in
-            // the above line, matchR[v] in the following
-            // recursive call will not get actor 'v' again
-            if (matchR[v] < 0 || bpm(bpGraph, matchR[v], seen, matchR)){
-                matchR[v] = u;
-                return true;
-            }
-        }
-    }
-    return false;
-}
-// Returns maximum number of matching from actresses to actors:
-int maxBPM(vector<vector<bool>>& bpGraph){
-
-    //matchR[i] is the actress number assigned to actor i
-    vector<int> matchR;
-    matchR.reserve(bpGraph.size());
-    //initialise the vector
-    for (int i = 0 ; i < bpGraph.size(); i++)
-        matchR.push_back(-1);  // the value -1 indicates nobody is assigned.
-
-    // Count of actors assigned to actresses
-    int result = 0;
-    for (int u = 0; u < bpGraph.size(); u++){
-        // Mark all actors as unseen for the next actress.
-        vector<bool> seen;
-        seen.reserve(bpGraph.size());
-        for (int i = 0 ; i < bpGraph.size(); i++)
-            seen.push_back(false);
-
-        // Find if the actresses 'u' can get a actor
-        if (bpm(bpGraph, u, seen, matchR))
-            result++;
-    }
-    return result;
-}
-
-int main()
-{
-    int total_actors, total_movies;
-    string total;
-    getline(cin,total);
-    stringstream (total) >> total_actors >> total_movies;
-
-    auto start = high_resolution_clock::now();
-    string name;
-    hash<string> hash; //this function hashes the string
-    // get actress names
-    for(int i = 0; i < total_actors; i++) {
-        getline(cin,name);
-        Actress actress = {name};
-        actresses.push_back(actress);
-    }
-
-    // get actor names
-    for (int i = 0; i < total_actors; i++) {
-        getline(cin,name);
-        Actor actor = {name};
-        actors.push_back(actor);
-    }
-
-    // set up movies/actors relationship
-    for(int i = 0; i < total_movies; i++) {
-        // get movie name
-        string title;
-        getline(cin,title);
-        // get cast size
-        int cast_size;
-        string cast_size_string;
-        getline(cin,cast_size_string);
-        cast_size = stoi(cast_size_string);
-        // populate actors hash-map and actresses movies
-        for (int j = 0; j < cast_size ; j++) {
-            getline(cin,name);
-            for (int k = 0; k < total_actors; k++) {
-                if(actresses[k].name == name)
-                    actresses[k].starredmovies.push_back(title);
-                if(actors[k].name == name) {
-                    actors[k].moviesMap.insert(pair<int,string>(hash(title),title));
+    dist[NIL] = INF;
+    while (!Q.empty()) {
+        u = Q.front();
+        Q.pop();
+        if (u != NIL) {
+            len = graph[u].size();
+            for (i = 0; i < len; i++) {
+                v = graph[u][i];
+                if (dist[match[v]] == INF) {
+                    dist[match[v]] = dist[u] + 1;
+                    Q.push(match[v]);
                 }
             }
         }
     }
-    /*
-    for(const auto& actress : actresses) {
-        cout << actress.name << endl;
-    }
-    for (const auto& actor : actors){
-        cout << actor.name << endl;
-    }
-    */
+    return (dist[NIL] != INF);
+}
 
-    //timestamp
-    auto stop1 = high_resolution_clock::now();
-    //cout << "Time taken to get the input: " <<  duration_cast<milliseconds>(stop1 - start).count() << endl;
-
-    // creation of the bipartite graph as a adjacency matrix made with vectors (crazy)
-    vector<vector<bool>> bpGraph;
-    for(const auto& actress : actresses) {
-        vector<bool> temp;
-        temp.reserve(actors.size());
-        for (const auto& actor : actors){
-            temp.push_back(costarred(actress, actor));
+bool dfs(int u) {
+    int i, v, len;
+    if (u != NIL) {
+        len = graph[u].size();
+        for (i = 0; i < len; i++) {
+            v = graph[u][i];
+            if (dist[match[v]] == dist[u] + 1) {
+                if (dfs(match[v])) {
+                    match[v] = u;
+                    match[u] = v;
+                    return true;
+                }
+            }
         }
-        bpGraph.push_back(temp);
+        dist[u] = INF;
+        return false;
+    }
+    return true;
+}
+
+int hopcroft_karp() {
+    int matching = 0, i;
+    // match[] is assumed NIL for all vertex in graph
+    while (bfs())
+        for (i = 1; i <= n; i++)
+            if (match[i] == NIL && dfs(i))
+                matching++;
+    return matching;
+}
+
+int main() {
+    int total_actors, total_movies;
+    hash<string> hash; //this function hashes the string
+    string total;
+    getline(cin, total);
+    stringstream(total) >> total_actors >> total_movies;
+    n = total_actors;
+
+    // get actress names
+    string name;
+    for (int i = 0; i < total_actors; i++) {
+        getline(cin, name);
+        actressesMap.insert(pair<int, int>(hash(name), i));
     }
 
-    //timestamp before algorithm
-    auto stop2 = high_resolution_clock::now();
-    //cout << "Time taken to fill the graph: " <<  duration_cast<milliseconds>(stop2 - stop1).count() << endl;
-    int res = maxBPM(bpGraph);
+    // get actor names
+    for (int i = 0; i < total_actors; i++) {
+        getline(cin, name);
+        actorsMap.insert(pair<int, int>(hash(name), i));
+    }
 
-    //timestamp after algorithm
-    auto stop3 = high_resolution_clock::now();
-    //cout << "Time taken to run the algorithm: " <<  duration_cast<milliseconds>(stop3 - stop2).count() << endl;
+    vector<int> actorTemp;
+    vector<int> actressTemp;
 
-    //output
-    if(res == total_actors)
-        cout <<"Mark";
+    // set up movies/actors relationship
+    string title;
+    int cast_size;
+    string cast_size_string;
+    for (int i = 0; i < total_movies; i++) {
+        // get movie name
+        getline(cin, title);
+        // get cast size
+        getline(cin, cast_size_string);
+        cast_size = stoi(cast_size_string);
+        // populate actors hash-map and actresses movies
+        for (int j = 0; j < cast_size; j++) {
+            getline(cin, name);
+            auto actressIt = actressesMap.find(hash(name));
+            if (actressIt != actressesMap.end()) {
+                actressTemp.push_back(actressesMap[hash(name)]);
+            } else {
+                actorTemp.push_back(actorsMap[hash(name)]);
+            }
+        }
+        //creation of the graph
+        for (const auto &actress : actressTemp) {
+            for (const auto &actor : actorTemp) {
+                graph[actress + 1].push_back(actor + total_actors + 1);
+            }
+        }
+        actorTemp.clear();
+        actressTemp.clear();
+    }
+
+    if (hopcroft_karp() == total_actors)
+        cout << "Mark";
     else
-        cout <<"Veronique";
+        cout << "Veronique";
+
     return 0;
 }
-//useful links:
-//https://cs.stackexchange.com/questions/118508/winning-strategy-for-a-given-game-on-graphs
-//http://discrete.openmathbooks.org/dmoi3/sec_matchings.html
-//https://www.geeksforgeeks.org/maximum-bipartite-matching/ this is the current implementation!!
-//https://www.geeksforgeeks.org/hopcroft-karp-algorithm-for-maximum-matching-set-1-introduction/ this is better but harder i think
-//https://www.geeksforgeeks.org/hopcroft-karp-algorithm-for-maximum-matching-set-2-implementation/
